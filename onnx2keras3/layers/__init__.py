fc716d20655915fb05f856625abe8f39da821360
ducoffeM <melanie.ducoffe@airbus.com>
2024-03-19 14:41:47 +0100

    init of layer module

---
+++ onnx2keras3/layers/__init__.py
@@ -0,0 +1,55 @@
+from onnx2keras3.layers.convolutional import convert_conv, convert_convtranspose
+from onnx2keras3.layers.activations import (
+    convert_relu,
+    convert_elu,
+    convert_lrelu,
+    convert_sigmoid,
+    convert_tanh,
+    convert_selu,
+    convert_exp,
+    convert_softmax,
+    convert_prelu,
+)
+from onnx2keras3.layers.operation import (
+    convert_clip,
+    convert_log,
+    convert_reduce_sum,
+    convert_reduce_mean,
+    convert_reduce_max,
+    convert_reduce_pow,
+    convert_reduce_sqrt,
+    convert_split,
+    convert_cast,
+    convert_floor,
+    convert_identity,
+    convert_argmax,
+    convert_reduce_l2,
+    convert_cast,
+    convert_slice,
+    convert_padding,
+)
+from onnx2keras3.layers.merging import (
+    convert_elementwise_add,
+    convert_elementwise_div,
+    convert_elementwise_mul,
+    convert_elementwise_sub,
+    convert_max,
+    convert_mean,
+    convert_min,
+)
+from onnx2keras3.layers.core import convert_gemm
+from onnx2keras3.layers.normalization import convert_batchnorm, convert_instancenorm, convert_dropout, convert_lrn
+from onnx2keras3.layers.pooling import convert_maxpool, convert_avgpool, convert_global_avg_pool
+from onnx2keras3.layers.reshaping import (
+    convert_transpose,
+    convert_shape,
+    convert_gather,
+    convert_concat,
+    convert_reshape,
+    convert_unsqueeze,
+    convert_flatten,
+    convert_squeeze,
+    convert_expand,
+    convert_upsample,
+)
+from onnx2keras3.layers.preprocessing import convert_resize
